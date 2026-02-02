662887d8e3195ca42d724e140ef198a85234bea7
feerbax56 <soki1997@mail.ru>
2026-02-02 16:07:24 +0500

    3

--- concurrency_go_tasks/02_channels/task1_producer_consumer/producer_consumer.go
+++ concurrency_go_tasks/02_channels/task1_producer_consumer/producer_consumer.go
@@ -1,10 +1,36 @@
 package producerconsumer
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"sync"
+)
 
 // Run запускает продюсера, который отправляет числа от 1 до 10, и консюмера,
 // который выводит их в writer. Используйте небуферизованный канал и ожидание
 // завершения горутин.
 func Run(w io.Writer) {
 	// TODO: реализовать продюсер и консюмер
+	ch := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		defer close(ch)
+
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for num := range ch {
+			fmt.Fprintf(w, "%d ", num)
+		}
+
+	}()
+	wg.Wait()
 }
