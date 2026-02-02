4ff750c6fc654c03b9251c17fca097c608f6c023
feerbax56 <soki1997@mail.ru>
2026-02-02 15:17:19 +0500

    PingPong done

--- concurrency_go_tasks/01_goroutines/task1_pingpong/main.go
+++ concurrency_go_tasks/01_goroutines/task1_pingpong/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 // PingPong должен запускать две горутины "ping" и "pong",
@@ -10,6 +12,44 @@ import (
 // Реализуйте синхронизацию через каналы и ожидание завершения.
 func PingPong(w io.Writer) {
 	// TODO: реализовать обмен сообщениями между горутинами
+	pingChan := make(chan struct{})
+	pongChan := make(chan struct{})
+	doneChan := make(chan struct{})
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		for i := 0; i < 5; i++ {
+			<-pingChan
+
+			fmt.Println("ping")
+
+			pongChan <- struct{}{}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		for i := 0; i < 5; i++ {
+			<-pongChan
+			fmt.Println("pong")
+
+			if i < 4 {
+				pingChan <- struct{}{}
+			}
+
+			close(doneChan)
+
+		}
+	}()
+	pingChan <- struct{}{}
+
+	<-doneChan
+	wg.Wait()
 }
 
 func main() {
