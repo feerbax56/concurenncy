88722da6df866e2675f62921c43e348dcfd32a79
Anton <soki1997@mail.ru>
2026-02-11 17:09:27 +0500

    03 t1 done

--- concurrency_go_tasks/03_composition/task1_pipeline/pipeline.go
+++ concurrency_go_tasks/03_composition/task1_pipeline/pipeline.go
@@ -1,7 +1,45 @@
 package pipeline
 
+import "sync"
+
 // Run строит конвейер из трёх стадий: квадрат, умножение на 2 и суммирование.
 func Run(nums []int) int {
 	// TODO: реализовать конвейер обработки чисел
-	return 0
+	kvadratCh := make(chan int)
+	multipleCh := make(chan int)
+	sumCh := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		defer close(kvadratCh)
+		for _, n := range nums {
+			kvadratCh <- n * n
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(multipleCh)
+
+		for n := range kvadratCh {
+			multipleCh <- n * 2
+		}
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(sumCh)
+
+		sum := 0
+		for num := range multipleCh {
+			sum += num
+		}
+
+		sumCh <- sum
+	}()
+	return <-sumCh
 }
