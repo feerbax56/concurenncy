c38043f7c4de2b783d722d56c178163c6064fd42
Anton <soki1997@mail.ru>
2026-02-12 09:47:50 +0500

    04 t1 done

--- concurrency_go_tasks/04_time/task1_scheduler/scheduler.go
+++ concurrency_go_tasks/04_time/task1_scheduler/scheduler.go
@@ -1,9 +1,46 @@
 package scheduler
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // Every запускает f каждые d и возвращает функцию для остановки.
 func Every(d time.Duration, f func()) (stop func()) {
 	// TODO: периодический вызов функции с возможностью остановки
-	return nil
+	var stoped bool
+	var mu sync.Mutex
+
+	stopCh := make(chan struct{})
+
+	go func() {
+		ticker := time.NewTicker(d)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				mu.Lock()
+				if stoped {
+					mu.Unlock()
+					return
+				}
+				mu.Unlock()
+				f()
+			case <-stopCh:
+				mu.Lock()
+				stoped = true
+				mu.Unlock()
+				return
+			}
+		}
+	}()
+	return func() {
+		mu.Lock()
+		defer mu.Unlock()
+		if !stoped {
+			stoped = true
+			close(stopCh)
+		}
+	}
 }
