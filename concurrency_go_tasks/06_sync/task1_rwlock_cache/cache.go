b4b57e1424f2915e22afeb546455661eeb30ca7e
Anton <soki1997@mail.ru>
2026-02-12 10:13:04 +0500

    06 t1 done

--- concurrency_go_tasks/06_sync/task1_rwlock_cache/cache.go
+++ concurrency_go_tasks/06_sync/task1_rwlock_cache/cache.go
@@ -11,16 +11,34 @@ type Cache struct {
 // New создаёт новый кэш.
 func New() *Cache {
 	// TODO: инициализировать структуру кэша
-	return &Cache{}
+	return &Cache{
+		data: make(map[string]interface{}),
+	}
 }
 
 // Set сохраняет значение по ключу.
 func (c *Cache) Set(key string, value interface{}) {
 	// TODO: реализовать запись с использованием RWMutex
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.data == nil {
+		c.data = make(map[string]interface{})
+	}
+
+	c.data[key] = value
 }
 
 // Get возвращает значение по ключу и признак его наличия.
 func (c *Cache) Get(key string) (interface{}, bool) {
 	// TODO: реализовать чтение с использованием RWMutex
-	return nil, false
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	if c.data == nil {
+		return nil, false
+
+	}
+
+	value, ok := c.data[key]
+	return value, ok
 }
