315aa4c44773d836a2c80bc6be379e0fa3a916d7
feerbax <soki1997@mail.ru>
2026-08-11 10:51:56 +0500

    fix timeout

--- concurrency_go_tasks/02_channels/task3_timeout_select/timeout.go
+++ concurrency_go_tasks/02_channels/task3_timeout_select/timeout.go
@@ -3,14 +3,28 @@ package timeout
 import (
 	"context"
 	"errors"
+	"time"
 )
 
-var ErrTimeout = errors.New("work timeout") // возвращается если работа заняла больше 100 мс
+var ErrTimeout = errors.New("work timeout")   // возвращается если работа заняла больше 100 мс
 var ErrCanceled = errors.New("work canceled") // возвращается при отмене контекста
 
 // Work выполняет длительную задачу и возвращает ошибку,
 // если она заняла больше 100 мс или контекст был отменён.
 func Work(ctx context.Context) error {
 	// TODO: реализовать через select и time.After
-	return nil
+	ch := make(chan error, 1)
+	go func() {
+		time.Sleep(150 * time.Millisecond)
+		ch <- nil
+	}()
+	select {
+	case err := <-ch:
+		return err
+	case <-time.After(100 * time.Millisecond):
+		ch <- ErrTimeout
+	case <-ctx.Done():
+		ch <- ErrCanceled
+	}
+	return <-ch
 }
