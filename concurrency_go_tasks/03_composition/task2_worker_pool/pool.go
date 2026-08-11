f25673f83fc7a8e202d54c88b4ee7b0a5ad04090
feerbax <soki1997@mail.ru>
2026-08-11 11:11:24 +0500

    fix pool

--- concurrency_go_tasks/03_composition/task2_worker_pool/pool.go
+++ concurrency_go_tasks/03_composition/task2_worker_pool/pool.go
@@ -1,10 +1,46 @@
 package pool
 
-import _ "sync"
+import (
+	"sync"
+)
 
 // RunPool обрабатывает задачи параллельно в заданном количестве воркеров
 // и возвращает сумму результатов.
 func RunPool(jobs []int, workers int) int {
+
+	if workers <= 0 {
+		workers = 1
+	}
 	// TODO: реализовать пул воркеров и сбор результатов
-	return 0
+	jobsChan := make(chan int, len(jobs))
+	resultChan := make(chan int, len(jobs))
+
+	var wg sync.WaitGroup
+	wg.Add(workers)
+
+	for i := 0; i < workers; i++ {
+		go func() {
+			defer wg.Done()
+			for job := range jobsChan {
+				resultChan <- job
+			}
+		}()
+	}
+
+	for _, job := range jobs {
+		jobsChan <- job
+	}
+	close(jobsChan)
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	sum := 0
+	for result := range resultChan {
+		sum += result
+	}
+	return sum
+
 }
